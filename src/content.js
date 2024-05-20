a8448e439cd3ae913bea761fa9caa57ebaa75138
kaiadachi <kai.adachi@up-sider.com>
2024-05-20 23:58:37 +0900

    update: 複数のボタンに対応できるように配列で保持するようにした

--- src/content.js
+++ src/content.js
@@ -7,55 +7,65 @@ function showDialog(callback, cancelCallback) {
     }
 }
 
-let isClick = false;
 function attachButtonListener(targetButton) {
+    targetButton.dataset.listenerAttached = 'true';
+    targetButton.dataset.clicked = '';
+    const handler = function (event) {
+        event.preventDefault();
+        if (!targetButton.dataset.clicked) {
+            targetButton.dataset.clicked = 'true';
+            showDialog(
+                () => {
+                    targetButton.removeEventListener('click', handler, true);
+                    targetButton.click();
+                    targetButton.dataset.clicked = '';
+                    targetButton.addEventListener('click', handler, true);
+                    console.log('Request executed');
+                },
+                () => {
+                    targetButton.dataset.clicked = '';
+                    event.stopPropagation();
+                    console.log('Request cancelled');
+                }
+            );
+        }
+    };
 
-    if (targetButton && !targetButton.dataset.listenerAttached) {
-        console.log('Listener attached');
-        targetButton.dataset.listenerAttached = 'true';
-
-        const handler = function (event) {
-            event.preventDefault();
-            if (!isClick) {
-                showDialog(
-                    () => {
-                        isClick = true;
-                        targetButton.click();
-                        isClick = false;
-                        console.log('Request executed');
-                    },
-                    () => {
-                        event.stopPropagation();
-                        console.log('Request cancelled');
-                    }
-                );
-            }
-        };
-
-        targetButton.addEventListener('click', handler, true);
-        targetButton.handler = handler;
-    }
+    targetButton.addEventListener('click', handler, true);
+    targetButton.handler = handler;
+    console.log('Listener attached');
 }
 
 function detachButtonListener(targetButton) {
-    if (targetButton && targetButton.dataset.listenerAttached === 'true') {
-        console.log('Listener detached');
+    if (targetButton.handler) {
         targetButton.removeEventListener('click', targetButton.handler, true);
         targetButton.dataset.listenerAttached = '';
         delete targetButton.handler;
+        console.log('Listener detached');
     }
 }
 
-const observer = new MutationObserver(() => {
-    const targetButton = document.querySelector('button[title="⌘ ⏎"].ui-btn.ui-btn-primary.ui-btn-compact-item.ui-btn-compact-first-item');
-    const env = document.querySelector('div.panel-group-panel-wrapper  div.ui-select-selector span.flex.items-center.truncate.block')
-    if (targetButton && env) {
-        if (env.getAttribute('title') === 'Production環境') {
-            attachButtonListener(targetButton);
-        } else if (env.getAttribute('title') !== 'Production環境' && targetButton.dataset.listenerAttached === 'true') {
-            detachButtonListener(targetButton);
-        }
-    }
+const observer = new MutationObserver((mutations) => {
+    mutations.forEach((mutation) => {
+        mutation.addedNodes.forEach((node) => {
+            if (node.nodeType === Node.ELEMENT_NODE) {
+                const env = document.querySelector('div.panel-group-panel-wrapper div.ui-select-selector span.flex.items-center.truncate.block');
+                const targetButtons = document.querySelectorAll('button[title="⌘ ⏎"].ui-btn.ui-btn-primary.ui-btn-compact-item.ui-btn-compact-first-item');
+
+                targetButtons.forEach(targetButton => {
+                    if (env && env.getAttribute('title') === 'Production環境') {
+                        if (!targetButton.dataset.listenerAttached) {
+                            attachButtonListener(targetButton);
+                        }
+                    } else {
+                        if (targetButton.dataset.listenerAttached === 'true') {
+                            detachButtonListener(targetButton);
+                        }
+                    }
+                });
+            }
+        });
+    });
 });
 
-observer.observe(document.body, {childList: true, subtree: true});
+observer.observe(document.body, {childList: true, subtree: true, attributes: true, attributeFilter: ['title']});
